89050af12b2b815983e1ca1478cd5e7fb1613a0f
AirsonVind <1185558990@qq.com>
2019-05-04 16:58:01 +0800

    commit all the files

---
+++ index.js
@@ -0,0 +1,25 @@
+// let delays = document.getElementsByClassName("delay");
+// let clients = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+// for (let i = 0;i < delays.length;i++){
+//     let divTop = new Array();
+//     divTop[i] = delays[i].getBoundingClientRect().top;
+// }
+// function DelayControl() {
+//     for (let i = 0;i < delays.length;i++){
+//         if (divTop <= clients) {
+//             delays.classList.add('delayon');
+//         }
+//     }
+//     window.onscroll = DelayControl;
+// }
+
+// window.onscroll = function(){
+//     var scrollY = window.scrollY;
+//     while (scrollY < 650){
+//         document.getElementsByClassName("back-to-top")[0].style.opacity = 0;
+//     }
+//     while (scrollY >= 650){
+//         document.getElementsByClassName("back-to-top")[0].style.opacity = 1;
+//     }
+// };
+//
